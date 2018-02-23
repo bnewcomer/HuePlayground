4f6bc9af6de9a80026bbabfe8cb8be26c22bdd7f
Benjamin Newcomer <bencnewcomer@gmail.com>
2018-02-23 13:51:50 -0800

    Initial Commit

---
+++ HuePlayground/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HuePlayground
+//
+//  Created by Benjamin Newcomer on 2/23/18.
+//  Copyright © 2018 Benjamin Newcomer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HuePlayground/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HuePlayground
+//
+//  Created by Benjamin Newcomer on 2/23/18.
+//  Copyright © 2018 Benjamin Newcomer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
